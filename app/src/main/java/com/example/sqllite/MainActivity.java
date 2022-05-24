6df413df5917531bb5cd1660897c9cfba244d27d
savanarsaaaaa <svnrsa28@gmail.com>
2022-05-24 15:11:05 +0700

    Activity 10

--- app/src/main/java/com/example/sqllite/MainActivity.java
+++ app/src/main/java/com/example/sqllite/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     String id,nm,tlp;
     private FloatingActionButton fab;
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static String url_select = "https://20200140088.praktikumtiumy.com/";
+    private static String url_select = "https://20200140088.praktikumtiumy.com/bacateman.php";
     public static final String TAG_ID = "id";
     public static final  String TAG_NAMA = "nama";
     public static final String TAG_TELPON = "telpon";
